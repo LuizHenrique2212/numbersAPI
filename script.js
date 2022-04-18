fb49911f1291bb7ccef7305cf9ba99fa5661be5c
Luiz Henrique <luizhenrique2205@gmail.com>
2022-04-18 19:21:40 -0300

    correção do erro do html

--- script.js
+++ script.js
@@ -8,19 +8,17 @@ const yearOption = document.querySelector('#year');
 function getAPIresponse() {
   let number = document.querySelector('.numero').value;
   if (number != '') {
-
     if (triviaOption.checked) option = '';
     if (mathOption.checked) option = 'math';
     if (dateOption.checked) option = 'date';
     if (yearOption.checked) option = 'year';
-
     const url = `http://numbersapi.com/${number}/${option}`;
     fetch(url)
       .then(response => response.text())
         .then(data => {
-         answer.innerHTML = data
-         answer.style.marginTop = '110px'
-      });
+         answer.innerHTML = data;
+         answer.style.marginTop = '110px';
+        });
   }
   else alert('Digite um número')
 }
